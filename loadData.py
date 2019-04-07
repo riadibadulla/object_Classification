6e6861e83e5c3f034a74e79008a504f545cd16f2
riadibadulla <ri31@pc5-036-l.cs.st-andrews.ac.uk>
2019-04-07 19:09:36 +0100

    Trains the model with logistic regression

--- loadData.py
+++ loadData.py
@@ -1,3 +1,19 @@
 import pandas as pd
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
 df = pd.read_csv("binary/X.csv")
-print(df[:])
+df = df.dropna()
+df_labels = pd.read_csv("binary/y.csv")
+print(df)
+print("----------------------")
+print(df.head())
+
+
+X_train, X_test, y_train, y_test = train_test_split(df, df_labels, test_size=0.33, random_state=42)
+
+logreg = LogisticRegression(C=1e5, solver='lbfgs', multi_class='ovr')
+logreg.fit(X_train,y_train)
+print(logreg.score(X_test,y_test))
