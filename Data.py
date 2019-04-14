13865097ebd9b96b6dfc130293fb63db2c256f47
riadibadulla <ri31@pc5-038-l.cs.st-andrews.ac.uk>
2019-04-14 20:35:49 +0100

    almost finished with part1

--- Data.py
+++ Data.py
@@ -1,14 +1,20 @@
-import pandas as pd
+import pandas as pd  
 import matplotlib.pyplot as plt
+from sklearn import decomposition
+import numpy as np
+
 class Data:
     df = None
     df_labels = None
     def __init__(self):
         self.df = pd.read_csv("binary/X.csv", header=None)
-        self.df = self.df.dropna()
         self.df_labels = pd.read_csv("binary/y.csv", header=None)
 
     def cutFeatures(self):
+        print("PORCO DIO")
+        print(self.df.loc[:,0])
+        print(self.df.loc[:,256])
+        print(self.df.loc[:,512])
         self.df = self.df.ix[:,:255]
 
     def plotGraphs(self, title, classNumber):
@@ -24,19 +30,59 @@ class Data:
         self.plotGraphs("Binary classification for book class",0)
         self.plotGraphs("Binary classification for plastic case class",1)
 
+    def plotHeatMap(self):
+        import seaborn as sns
+        corr = self.df.corr()
+        sns.heatmap(corr, xticklabels=corr.columns.values, yticklabels=corr.columns.values, annot = True)
+        plt.title("Correlation matrix")
+        plt.xlabel("Features")
+        plt.ylabel("Features")
+        plt.show()
 
-    def plotCorrelation(self):
+        return corr
+
+    def delteCorrelatedFeatures(self):
         array = []
         corr = self.df.corr()
+        print(corr)
         for i in range(len(corr)):
             for j in range(i+1, len(corr)):
                 if (corr[i][j] >= 0.90):
-                    #print(i,"  ",j)
                     array.append(i)
         array = list(set(array))
-        print(array)
-        #self.df = self.df.drop(i,1)
-        #print(corr)
         for i in array:
             self.df = self.df.drop(i,1)
-        print("Number of features:", len(self.df.columns))
+        print("Features left:", self.df.columns)
+
+    def cleanData(self):
+        for i in self.df.index:
+            for j in self.df.columns:
+                if (self.df.isna().loc[i,j]==True):
+                    self.df.drop(i,0)
+                    self.df_labels.drop(i,0)
+    
+    def applyPCA(self,number):
+        pca = decomposition.PCA(n_components=number)
+        self.df = pd.DataFrame(pca.fit_transform(self.df))
+        print(self.df)
+    
+    def visualiseOn2D(self):
+        if (len(self.df.columns) != 2):
+            self.applyPCA(2)
+
+        redElements = []
+        blueElements = []
+        for index, row in self.df.iterrows():
+            if self.df_labels.loc[index,0] == 0:
+                redElements.append(row)
+            else:
+                blueElements.append(row)
+        redElements = pd.DataFrame(redElements)
+        blueElements = pd.DataFrame(blueElements)
+        plt.scatter(redElements.loc[:,0],redElements.loc[:,1], color="red")
+        plt.scatter(blueElements.loc[:,0],blueElements.loc[:,1],color="blue")
+        plt.grid(b=True)
+        plt.title("Feature against feature graph after PCA applied")
+        plt.xlabel("Generated Feature 1")
+        plt.ylabel("Generated Feature 2")
+        plt.show()
--- loadData.py
+++ loadData.py
@@ -4,23 +4,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from Data import Data
-import os
 
-os.system('clear')
+X_train = None
+X_test = None
+y_train = None
+y_test = None
 
-#import Data from Data
-
-data = Data()
-#data.cutFeatures()
-print(data.df)
-print("----------------------")
-print(data.df.head())
-
-data.visulalise()
-data.plotCorrelation()
-print(data.df)
-
-X_train, X_test, y_train, y_test = train_test_split(data.df, data.df_labels, test_size=0.3, random_state=4)
+def clearScreen():
+    import os
+    os.system('clear')
 
 def logRegression():
     logreg = LogisticRegression(C=1e5, solver='lbfgs', multi_class='ovr')
@@ -28,9 +20,24 @@ def logRegression():
     print(logreg.score(X_test,y_test))
 
 def neuralNets():
-    mlp = MLPClassifier(hidden_layer_sizes=(50,), activation="logistic", verbose=True, max_iter=500, early_stopping=True, validation_fraction=0.3)
+    mlp = MLPClassifier(hidden_layer_sizes=(500,), activation="relu", verbose=True, max_iter=500, early_stopping=True, validation_fraction=0.3)
     mlp.fit(X_train,y_train)
     print(mlp.score(X_test,y_test))
 
-logRegression()
-neuralNets()
+def main():
+    global X_train, X_test, y_train, y_test
+    clearScreen()
+    data = Data()
+    #data.cleanData()
+    #data.applyPCA(2)
+    #data.visualiseOn2D()
+    #data.visulalise()
+    data.cutFeatures()
+    data.delteCorrelatedFeatures()
+    data.plotHeatMap()
+    print(data.df)
+    #X_train, X_test, y_train, y_test = train_test_split(data.df, data.df_labels, test_size=0.3, random_state=4)
+    #logRegression()
+    # neuralNets()
+
+main()
